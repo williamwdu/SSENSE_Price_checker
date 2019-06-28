af8666c50487ac57b2a64eed826d9aa65f65f3ef
Wei Du <williamdu@me.com>
2019-06-27 23:30:36 -0400

    Add size tracking functionality, support 2019 sale

--- pricemonitor/pricemonitor/Program.cs
+++ pricemonitor/pricemonitor/Program.cs
@@ -8,6 +8,7 @@ using System.Net;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Net.Mail;
+using OpenQA.Selenium.Firefox;
 
 namespace pricemonitor
 {
@@ -17,7 +18,7 @@ namespace pricemonitor
         static void Main(string[] args)
         {
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(@"C:\Git\SSENSE_Price_checker\data.xlsx");
+            Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(@"D:\Github\SSENSE_Price_checker\data.xlsx");
             Excel._Worksheet xlWorksheet = xlWorkBook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
             int rowCount = xlRange.Rows.Count;
@@ -25,10 +26,10 @@ namespace pricemonitor
             List<bontique> buylist = new List<bontique>();
             for (int i = 1; i <= rowCount; i++)
             {
-                bontique tmp = new bontique() { row = i, url = xlRange.Cells[i, 1].Text, lowest= Convert.ToDouble(xlRange.Cells[i, 2].Text), highest= Convert.ToDouble(xlRange.Cells[i, 3].Text), last = Convert.ToDouble(xlRange.Cells[i, 4].Text) };
+                bontique tmp = new bontique() { row = i, url = xlRange.Cells[i, 1].Text, lowest= Convert.ToDouble(xlRange.Cells[i, 2].Text), highest= Convert.ToDouble(xlRange.Cells[i, 3].Text), last = Convert.ToDouble(xlRange.Cells[i, 4].Text), size = xlRange.Cells[i, 5].Text };
                 buylist.Add(tmp);
             }
-            IWebDriver driver = new ChromeDriver(@"C:\Git\SSENSE_Price_checker");
+            IWebDriver driver = new FirefoxDriver(@"D:\Github\SSENSE_Price_checker");
 
             foreach (bontique tmp1 in buylist)
             {
@@ -45,13 +46,40 @@ namespace pricemonitor
                 IWebElement pricetag = driver.FindElement(By.XPath("//span[@class='price']"));
                 string ooo = pricetag.GetAttribute("innerHTML").ToString();
                 double price = Convert.ToDouble(ooo.Replace(" CAD", "").Replace("$",""));
+                IWebElement sizeelement = driver.FindElement(By.XPath("//select[@id='size']"));
+                string sizeString = sizeelement.Text;
+                string[] sizeList = sizeString.Split(
+                    new[] { "\r\n", "\r", "\n" },
+                    StringSplitOptions.None
+                );
                 xlRange.Cells[tmp1.row, 4].Value = price;
-                
+                //only xx left, sold out, normal
+                foreach(string tmpsize in sizeList)
+                {
+                    if (tmpsize.Contains(tmp1.size))
+                    {
+                        if (tmpsize.Contains("Out"))
+                        {
+                            //sold out send notification
+                            email(tmp1, 3);
+                        }
+                        else if (tmpsize.Contains("left"))
+                        {
+                            //only serveral left buybuybuy!
+                            email(tmp1, 2);
+
+                        }
+                        else
+                        {
+                            //do nothing
+                        }
+                    }
+                }
                 if (price < tmp1.lowest)
                 {
                     xlRange.Cells[tmp1.row, 2].Value = price;
                     tmp1.lowest = price;
-                    email(tmp1);
+                    email(tmp1,1);
                 }
                 if (price > tmp1.highest)
                 {
@@ -67,17 +95,40 @@ namespace pricemonitor
         }
 
 
-        static void email(bontique a)
+        static void email(bontique a, int type)
         {
 
             var clientstmp = new SmtpClient("smtp.gmail.com", 587)
             {
+                //gmail support
                 Credentials = new NetworkCredential("viarailtest333@gmail.com", "innovation123"),
                 EnableSsl = true
             };
-            MailMessage mail = new MailMessage("viarailtest333@gmail.com", "williamdu@me.com", "Price Changed!", a.url + "        Now the price is " + a.lowest + "        highest price is " + a.highest);
-            clientstmp.Send(mail);
+            //MailMessage(from,to,...)
+            if (type == 1)
+            {
+                MailMessage mail = new MailMessage("viarailtest333@gmail.com", "williamdu@me.com", "Price Changed!", a.url + "        Now the price is " + a.lowest + "        highest price is " + a.highest);
+                clientstmp.Send(mail);
+            }
+            else if (type == 2)
+            {
+                MailMessage mail = new MailMessage("viarailtest333@gmail.com", "williamdu@me.com", "Few left!", a.url + "        Now the price is " + a.lowest + "        highest price is " + a.highest);
+                clientstmp.Send(mail);
+            }
+            else if (type == 3)
+            {
+                MailMessage mail = new MailMessage("viarailtest333@gmail.com", "williamdu@me.com", "Sold Out!", a.url + "        Now the price is " + a.lowest + "        highest price is " + a.highest);
+                clientstmp.Send(mail);
+            }
+            else
+            {
+                //illegal type
+                MailMessage mail = new MailMessage("viarailtest333@gmail.com", "williamdu@me.com", "Price Changed!", a.url + "        Now the price is " + a.lowest + "        highest price is " + a.highest);
+                clientstmp.Send(mail);
+            }
         }
+
+
     }
 
     class bontique
@@ -87,6 +138,7 @@ namespace pricemonitor
         public Double lowest { get; set; } //2
         public Double highest { get; set; } //3
         public Double last { get; set; } //4
+        public String size { get; set; } //5
 
     }
 }
